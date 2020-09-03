ddef13a3fecab0229c04c66e0ad9439d02ce5878
Thai Do <thai.hl.do@gmail.com>
2020-09-03 14:57:42 -0700

    refactored getUserByEmail function

--- helpers.js
+++ helpers.js
@@ -1,10 +1,10 @@
 const getUserByEmail = (email, database) => {
   for (const user in database) {
     if (database[user].email === email) {
-      return user;
+      return database[user];
     }
   }
-  return null;
+  return undefined;
 };
 
 module.exports = { getUserByEmail };
