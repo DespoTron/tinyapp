f4d8fac45165bca3c51f79b7582b9f5f75efddf7
Thai Do <thai.hl.do@gmail.com>
2020-09-03 15:48:29 -0700

    moved all helper functions into helpers.js

--- helpers.js
+++ helpers.js
@@ -7,4 +7,19 @@ const getUserByEmail = (email, database) => {
   return undefined;
 };
 
-module.exports = { getUserByEmail };
+// Generate a string of 6 random alphanumeric characters
+const generateRandomString = () => {
+  return Math.random().toString(20).substr(2, 6);
+};
+
+const urlsForUser = (id, urlDatabase) => {
+  const userURLDatabase = {};  
+  for (const url in urlDatabase) {
+    if(urlDatabase[url].userID === id) {
+      userURLDatabase[url] = urlDatabase[url];
+    }
+  }
+  return userURLDatabase;
+};
+
+module.exports = { getUserByEmail, generateRandomString, urlsForUser };
